54efbc6217cbb3dd683bf89ceffc0a7cb045d0e5
María Delgado <mariad1996@gmail.com>
2023-11-08 23:37:02 +0100

    started the script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+
+ const boton = document.getElementById('boton');
+ const lista = document.getElementById('lista');
+
+ boton.addEventListener('click', function () {
+    const nuevoItem = prompt('Escribe el nuevo elemento de la lista');
+    const li = lista.createElement();
+    lista.appendChild();
+ })
+
+
