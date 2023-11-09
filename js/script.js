b660738172f83c6cde6ff93395624b3f6d62c425
María Delgado <mariad1996@gmail.com>
2023-11-09 16:38:33 +0100

    Finished

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
- // Aquí tu código
+// Aquí tu código
 
- const boton = document.getElementById('boton');
- const lista = document.getElementById('lista');
+const agregar = document.getElementById('agregar');
+const lista = document.getElementById('lista');
 
- boton.addEventListener('click', function () {
-    const nuevoItem = prompt('Escribe el nuevo elemento de la lista');
-    const li = lista.createElement();
-    lista.appendChild();
- })
+agregar.addEventListener('click', function () {
+   const item = document.createElement('li');
+   const texto = prompt('Escribe el nuevo elemento de la lista');
+   item.textContent = texto;
+   lista.appendChild(item);
+})
 
+// Esto no está en el ejercicio, pero me parecía lógico por si te equivocas o quieres volver a empezar sin recargar.
+
+const borrarUno = document.getElementById('borrarUno');
+const clear = document.getElementById('clear');
+
+borrarUno.addEventListener('click', function () {
+   if (lista.hasChildNodes() == true) {
+      lista.removeChild(lista.lastChild);
+   }
+})
+
+clear.addEventListener('click', function () {
+   if (lista.hasChildNodes() == true) {
+      lista.innerHTML='';
+   }
+})
 
